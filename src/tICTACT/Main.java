fdd2f94221f93a78d318c96c528e01b2511c7651
vskolll <lacunas.basest-0i@icloud.com>
2024-12-02 10:29:11 +0300

    Й

--- src/tICTACT/Main.java
+++ src/tICTACT/Main.java
@@ -5,59 +5,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        TicTacToe game = new TicTacToe();
-
         System.out.println("Ласкаво просимо до гри Хрестики-Нулики!");
+
+        int size = chooseBoardSize(scanner);
         int gameMode = chooseGameMode(scanner);
 
-        while (true) {
-            game.printBoard();
+        GameController controller = new GameController(size, gameMode);
+        controller.startGame(scanner);
 
-            if (gameMode == 1) { // Гравець проти комп'ютера
-                if (game.isPlayerTurn()) {
-                    System.out.println("Ваш хід:");
-                    game.playerMove(scanner);
-                } else {
-                    System.out.println("Хід комп'ютера:");
-                    game.computerMove();
-                }
-            } else if (gameMode == 2) { // Гравець проти гравця
-                System.out.println(game.isPlayerTurn() ? "Хід першого гравця (X):" : "Хід другого гравця (O):");
-                game.playerMove(scanner);
-            }
-
-            if (game.checkWin()) {
-                game.printBoard();
-                System.out.println(game.getWinnerMessage());
-                break;
-            }
-
-            if (game.isBoardFull()) {
-                game.printBoard();
-                System.out.println("Нічия!");
-                break;
-            }
-
-            game.switchTurn();
-        }
         scanner.close();
     }
 
+    private static int chooseBoardSize(Scanner scanner) {
+        while (true) {
+            try {
+                System.out.println("Оберіть розмір поля:");
+                System.out.println("1. Стандартне поле 3x3");
+                System.out.println("2. Велике поле 15x15");
+                int choice = scanner.nextInt();
+                if (choice == 1) return 3;
+                if (choice == 2) return 15;
+                System.out.println("Будь ласка, оберіть 1 або 2.");
+            } catch (Exception e) {
+                System.out.println("Помилка вводу! Спробуйте ще раз.");
+                scanner.nextLine();
+            }
+        }
+    }
+
     private static int chooseGameMode(Scanner scanner) {
-        int mode;
         while (true) {
             try {
                 System.out.println("Оберіть режим гри:");
                 System.out.println("1. Гравець проти комп'ютера");
                 System.out.println("2. Гравець проти гравця");
-                mode = scanner.nextInt();
-                if (mode == 1 || mode == 2) break;
-                else System.out.println("Будь ласка, оберіть 1 або 2.");
+                int mode = scanner.nextInt();
+                if (mode == 1 || mode == 2) return mode;
+                System.out.println("Будь ласка, оберіть 1 або 2.");
             } catch (Exception e) {
                 System.out.println("Помилка вводу! Введіть число 1 або 2.");
-                scanner.nextLine(); // Очищення буфера вводу
+                scanner.nextLine();
             }
         }
-        return mode;
     }
 }
--- src/tICTACT/TicTacToe.java
+++ src/tICTACT/TicTacToe.java
@@ -3,51 +3,35 @@ package tICTACT;
 import java.util.Scanner;
 
 public class TicTacToe {
-    private final char[][] board = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
-    };
+    private final Board board;
     private final char PLAYER1 = 'X';
     private final char PLAYER2 = 'O';
+    private final int winCondition;
     private boolean isPlayerTurn = true;
 
+    public TicTacToe(int size) {
+        this.board = new Board(size);
+        this.winCondition = size == 15 ? 5 : 3;
+    }
+
     public void printBoard() {
-        System.out.println("  1 2 3");
-        for (int i = 0; i < board.length; i++) {
-            System.out.print((i + 1) + " ");
-            for (char cell : board[i]) {
-                System.out.print(cell + " ");
-            }
-            System.out.println();
-        }
+        board.print();
     }
 
     public boolean isBoardFull() {
-        for (char[] row : board) {
-            for (char cell : row) {
-                if (cell == ' ') return false;
-            }
-        }
-        return true;
+        return board.isFull();
     }
 
     public void playerMove(Scanner scanner) {
-        int row, col;
         while (true) {
             try {
-                System.out.print("Введіть рядок (1-3): ");
-                row = scanner.nextInt() - 1;
-                System.out.print("Введіть стовпець (1-3): ");
-                col = scanner.nextInt() - 1;
+                System.out.print("Введіть рядок (1-" + board.getSize() + "): ");
+                int row = scanner.nextInt() - 1;
+                System.out.print("Введіть стовпець (1-" + board.getSize() + "): ");
+                int col = scanner.nextInt() - 1;
 
-                char currentPlayer = isPlayerTurn ? PLAYER1 : PLAYER2;
-                if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
-                    board[row][col] = currentPlayer;
-                    break;
-                } else {
-                    System.out.println("Некоректний хід, спробуйте ще раз.");
-                }
+                if (board.setCell(row, col, getCurrentPlayer())) break;
+                else System.out.println("Некоректний хід, спробуйте ще раз.");
             } catch (Exception e) {
                 System.out.println("Помилка вводу! Спробуйте ще раз.");
                 scanner.nextLine();
@@ -56,27 +40,16 @@ public class TicTacToe {
     }
 
     public void computerMove() {
-        int[] bestMove = Minimax.findBestMove(board, PLAYER2, PLAYER1);
-        board[bestMove[0]][bestMove[1]] = PLAYER2;
+        int[] bestMove = Minimax.findBestMove(board.getBoard(), PLAYER2, PLAYER1);
+        board.setCell(bestMove[0], bestMove[1], PLAYER2);
     }
 
     public boolean checkWin() {
-        return checkWinFor(PLAYER1) || checkWinFor(PLAYER2);
+        return board.checkWin(getCurrentPlayer(), winCondition);
     }
 
     public String getWinnerMessage() {
-        if (checkWinFor(PLAYER1)) return "Перший гравець (X) переміг!";
-        if (checkWinFor(PLAYER2)) return isPlayerTurn ? "Комп'ютер (O) переміг!" : "Другий гравець (O) переміг!";
-        return "Нічия!";
-    }
-
-    private boolean checkWinFor(char symbol) {
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
-            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) return true;
-        }
-        return board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol
-                || board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol;
+        return isPlayerTurn ? "Перший гравець (X) переміг!" : "Другий гравець (O) переміг!";
     }
 
     public boolean isPlayerTurn() {
@@ -85,5 +58,9 @@ public class TicTacToe {
 
     public void switchTurn() {
         isPlayerTurn = !isPlayerTurn;
+    }
+
+    private char getCurrentPlayer() {
+        return isPlayerTurn ? PLAYER1 : PLAYER2;
     }
 }
